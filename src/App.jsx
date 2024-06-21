7a18dfee24119cebab6fffc91fbd922a028a83b7
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-06-21 20:12:52 +0530

    auto slider implement

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Home from "./pages/home";
 import NavBar from "./components/navbar";
 function App() {
   return (
-    <>
+    <main>
       <NavBar />
       <Home />
-    </>
+    </main>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
   return (
     <main className="home__main">
+      <Greet />
+      <AgricultureSustainblity />
+    </main>
+  );
+};
+const Greet = () => {
+  return (
+    <section className="greets__section">
       <article className="greets">
         <h1 className="greet__welcome">WELCOME TO</h1>
         <span className="greet__name">VEL FARMS</span>
@@ -10,7 +20,72 @@ const Home = () => {
         </p>
         <button className="greet__btn">Learn More</button>
       </article>
-    </main>
+    </section>
+  );
+};
+const AgricultureSustainblity = () => {
+  return (
+    <section className="agriculture_section">
+      <h1 className="heading__primary">
+        Agricultural <span className="secondary__color">Sustainability</span>
+      </h1>
+      <div className="ag-container">
+        <div className="coconut__galary">
+          <div className="slides">
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-1.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-2.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-3.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-4.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-5.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-6.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+            <div className="galary-img-box">
+              <img
+                src="/assets/galary/galary-7.webp"
+                alt="farms-log"
+                className="galary-img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="ag-practice"></div>
+      </div>
+    </section>
   );
 };
 export default Home;
