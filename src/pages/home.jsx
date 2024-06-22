1dd8c7bd29d7bfc5b7fe5eb348038359d921060a
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-06-22 11:03:53 +0530

    animation updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -81,9 +81,56 @@ const AgricultureSustainblity = () => {
             </div>
           </div>
         </div>
-        <div className="ag-practice"></div>
+        <div className="ag-practice">
+          <div className="ag-articles">
+            <div className="ag-titile">
+              {" "}
+              <h3>Commitment to Quality</h3>
+            </div>
+            <p className="ag-para">
+              At Vel Farms, we are unwavering in our steadfast dedication to
+              delivering the highest quality coconut products. From planting to
+              harvesting, we rigorously adhere to the most stringent standards.
+              Every step of our meticulous and comprehensive process is
+              diligently managed to ensure that our products remain pure,
+              natural, and abundantly packed with essential nutrients for our
+              valued customers.
+            </p>
+          </div>
+          <div className="ag-articles">
+            <div className="ag-titile">
+              {" "}
+              <h3>Impact on the environment</h3>
+            </div>
+            <p className="ag-para">
+              At Vel Farms, we are committed to practicing sustainable coconut
+              farming, actively promoting biodiversity and supporting local
+              wildlife. Our eco-friendly methods, which encompass efficient
+              irrigation systems and organic agricultural practices, are
+              designed to deliver the highest quality coconut products. By
+              employing these sustainable techniques, we also contribute to the
+              reduction of greenhouse gas emissions, thereby fostering a
+              healthier environment for future generations
+            </p>
+          </div>
+          <div className="ag-articles">
+            <div className="ag-titile">
+              {" "}
+              <h3> Modern farming techniques</h3>
+            </div>
+            <p className="ag-para">
+              At Vel Farms, we seamlessly integrate traditional wisdom with
+              modern innovations, such as precision agriculture and advanced
+              irrigation systems, to enhance both productivity and
+              sustainability. This harmonious blend of time-honored practices
+              and cutting-edge technology ensures that we consistently deliver
+              superior coconut products to our customers.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
 };
+
 export default Home;
