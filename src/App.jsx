b9b77d30c47d88428c39459e47a683902b40a0c5
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-06-28 08:31:51 +0530

    nav bar modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Contact from "./pages/contact";
 import Farm from "./pages/farm";
 import Product from "./pages/product";
 import About from "./pages/about";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/farm" element={<Farm />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/farm" element={<Farm />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
